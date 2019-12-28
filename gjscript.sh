bf23379133a86f3be9310442a3d0987593e1ebde
Anna Kolesnichenko <anna-lacrymosa@yandex.ru>
2019-12-28 17:12:32 +0100

    Create gjscript.sh

---
+++ gjscript.sh
@@ -0,0 +1,2 @@
+docker run -d --name gerrit --restart always -p 8080:8080 -p 29418:29418 gerritcodereview/gerrit
+docker run -d --name jenkins --restart always -p 8090:8080 -p 50000:50000 -e JAVA_OPTS=-Djenkins.install.runSetupWizard=false docker.io/jenkins/jenkins
